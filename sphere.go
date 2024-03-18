3cc7099a694d511396c81058706cf40f6ee6cf0a
Ivan Grega <grega.ivan@gmail.com>
2024-03-18 14:16:46 +0000

    Concurrent version running

--- sphere.go
+++ sphere.go
@@ -1,37 +1,80 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/png"
 	"math"
 	"os"
+	"sync"
+	"time"
 )
 
+const res = 512
+
 func density(x, y, z float64) float64 {
+	x0, y0, z0 := 0.0, 0.0, 3.0
+	x = x - x0
+	y = y - y0
+	z = z - z0
 	r := math.Sqrt((x * x) + (y * y) + (z * z))
-	if r < 0.75 {
+	if r < 0.25 {
+		return 0.0
+	} else if r < 0.75 {
 		return 0.01
 	} else {
 		return 0
 	}
 }
 
+func integrate_along_ray(vx, vy, vz, ds, smin, smax float64) float64 {
+	// normalize components of the ray
+	norm := math.Sqrt(vx*vx + vy*vy + vz*vz)
+	vx /= norm
+	vy /= norm
+	vz /= norm
+	// integrate
+	T := 0.0
+	for s := smin; s < smax; s += ds {
+		x := vx * s
+		y := vy * s
+		z := vz * s
+		T += density(x, y, z) * ds
+	}
+	return math.Exp(-T)
+}
+
+func computePixel(img *[res][res]float64, i, j int, x, y, z float64, ds, smin, smax float64, wg *sync.WaitGroup) {
+	defer wg.Done()
+	img[i][j] = integrate_along_ray(x, y, z, ds, smin, smax)
+}
+
+func timer() func() {
+	start := time.Now()
+	return func() {
+		fmt.Println(time.Since(start))
+	}
+}
+
 func main() {
-	const res = 256
+	defer timer()()
 	var img [res][res]float64
+	// concurrent version:
+	var wg sync.WaitGroup
+	for i := 0; i < res; i++ {
+		for j := 0; j < res; j++ {
+			wg.Add(1)
+			go computePixel(&img, i, j, float64(i)/(res/2)-1, float64(j)/(res/2)-1, 2.0, 0.001, 2, 5, &wg)
+		}
+	}
+	wg.Wait()
+
 	_max, _min := 0.0, 1.0
 	for i := 0; i < res; i++ {
 		for j := 0; j < res; j++ {
-			x := float64(i)/(res/2) - 1
-			y := float64(j)/(res/2) - 1
-			T := 0.0
-			for k := 0; k < res; k++ {
-				z := float64(k)/(res/2) - 1
-				dz := 2.0 / res
-				T += density(x, y, z) * dz
-			}
-			img[i][j] = math.Exp(-T)
+			// non-concurrent version:
+			// img[i][j] = integrate_along_ray(float64(i)/(res/2)-1, float64(j)/(res/2)-1, 2.0, 0.001, 0, 5)
 			if img[i][j] > _max {
 				_max = img[i][j]
 			}
