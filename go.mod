51aec4956a93743811c9ffb795666462ae79388a
Ivan Grega <ig348@cam.ac.uk>
2024-03-31 13:50:15 +0200

    Trying to implement a few better integration methods.
    However, struggling to get an improvement in speed.

--- go.mod
+++ go.mod
@@ -5,12 +5,13 @@ go 1.22.1
 require (
 	github.com/go-gl/mathgl v1.1.0
 	github.com/schollz/progressbar/v3 v3.14.2
+	gonum.org/v1/gonum v0.15.0
 )
 
 require (
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/image v0.0.0-20190321063152-3fc05d484e9f // indirect
+	golang.org/x/image v0.14.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/term v0.18.0 // indirect
 )
--- sphere.go
+++ sphere.go
@@ -12,12 +12,14 @@ import (
 
 	"github.com/go-gl/mathgl/mgl64"
 	"github.com/schollz/progressbar/v3"
+	"gonum.org/v1/gonum/integrate/quad"
 )
 
 const res = 512
-const fov = 1.0
-const R = 100.0
-const num_images = 91
+const fov = 10.0
+const R = 10.0
+const num_images = 2
+const flat_field = 0.1
 
 func density(x, y, z float64) float64 {
 	x0, y0, z0 := 0.0, 0.0, 0.0
@@ -54,7 +56,7 @@ func integrate_along_ray(origin, direction mgl64.Vec3, ds, smin, smax float64) f
 	// normalize components of the ray
 	direction = direction.Normalize()
 	// integrate
-	T := 0.0
+	T := flat_field
 	for s := smin; s < smax; s += ds {
 		x := origin[0] + direction[0]*s
 		y := origin[1] + direction[1]*s
@@ -64,8 +66,65 @@ func integrate_along_ray(origin, direction mgl64.Vec3, ds, smin, smax float64) f
 	return math.Exp(-T)
 }
 
+func integrate_hierarchical(origin, direction mgl64.Vec3, ds, smin, smax float64) float64 {
+	// normalize components of the ray
+	direction = direction.Normalize()
+	// integrate
+	T := 0.0
+	for s := smin; s <= smax; s += 9 * ds {
+		x := origin[0] + direction[0]*s
+		y := origin[1] + direction[1]*s
+		z := origin[2] + direction[2]*s
+		rho := density(x, y, z)
+		if rho > 0 {
+			for _s := s - 4*ds; _s <= s+4*ds; _s += ds {
+				x := origin[0] + direction[0]*_s
+				y := origin[1] + direction[1]*_s
+				z := origin[2] + direction[2]*_s
+				T += density(x, y, z) * ds
+			}
+		}
+	}
+	return math.Exp(-T)
+}
+
+func integrate_adaptive(origin, direction mgl64.Vec3, smin, smax float64) float64 {
+	// normalize components of the ray
+	direction = direction.Normalize()
+	f := func(s float64) float64 {
+		x := origin[0] + direction[0]*s
+		y := origin[1] + direction[1]*s
+		z := origin[2] + direction[2]*s
+		return density(x, y, z)
+	}
+	return math.Exp(-adaptive_quad(f, smin, smax, 100))
+}
+
+func adaptive_quad(f func(float64) float64, xmin, xmax float64, n int) float64 {
+	// integrate f from xmin to xmax
+	// split into 2 intervals and evaluate the integral of each interval
+	xmid := (xmin + xmax) / 2
+	// n2 := int(math.Ceil(float64(n) / 2))
+	n2 := (n + 1) / 2 // should be the same as above
+	int1 := quad.Fixed(f, xmin, xmid, n2, nil, 0)
+	int2 := quad.Fixed(f, xmid, xmax, n2, nil, 0)
+	// new resolution
+	_int1 := quad.Fixed(f, xmin, xmid, n, nil, 0)
+	_int2 := quad.Fixed(f, xmid, xmax, n, nil, 0)
+	thresh := 1e-5
+	if math.Abs(_int1-int1) > thresh {
+		_int1 = adaptive_quad(f, xmin, xmid, n)
+	}
+	if math.Abs(_int2-int2) > thresh {
+		_int2 = adaptive_quad(f, xmid, xmax, n)
+	}
+	return _int1 + _int2
+}
+
 func computePixel(img *[res][res]float64, i, j int, origin, direction mgl64.Vec3, ds, smin, smax float64, wg *sync.WaitGroup) {
 	defer wg.Done()
+	// img[i][j] = integrate_adaptive(origin, direction, smin, smax)
+	// img[i][j] = integrate_hierarchical(origin, direction, ds, smin, smax)
 	img[i][j] = integrate_along_ray(origin, direction, ds, smin, smax)
 }
 
@@ -83,7 +142,7 @@ func main() {
 	bar := progressbar.Default(int64(num_images))
 	for i_img := 0; i_img < num_images; i_img++ {
 		dth := 180.0 / (num_images - 1)
-		th := float64(i_img) * dth
+		th := float64(i_img)*dth + 10.0
 		bar.Add(1)
 		// zero out img
 		for i := 0; i < res; i++ {
@@ -111,20 +170,20 @@ func main() {
 		}
 		wg.Wait()
 
-		if th == 0 {
-			_max, _min := 0.0, 1.0
-			for i := 0; i < res; i++ {
-				for j := 0; j < res; j++ {
-					if img[i][j] > _max {
-						_max = img[i][j]
-					}
-					if img[i][j] < _min {
-						_min = img[i][j]
-					}
-				}
-			}
-			fmt.Println(_max, _min)
-		}
+		// if th == 0 {
+		// 	_max, _min := 0.0, 1.0
+		// 	for i := 0; i < res; i++ {
+		// 		for j := 0; j < res; j++ {
+		// 			if img[i][j] > _max {
+		// 				_max = img[i][j]
+		// 			}
+		// 			if img[i][j] < _min {
+		// 				_min = img[i][j]
+		// 			}
+		// 		}
+		// 	}
+		// 	fmt.Println(_max, _min)
+		// }
 
 		myImage := image.NewRGBA(image.Rect(0, 0, res, res))
 		for i := 0; i < res; i++ {
